abfbad3b557400abffa1899db5a05006039964df
Hans Han <hanshan@MacBook-Pro.local>
2025-07-21 17:38:54 +1000

    updated script to print out version of pandas

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import flask
+import pandas
 
 def main():
     print("Hello from test-uv!")
-
+    pandas_v = pandas.__version__
+    print(f"pandas version: {pandas_v}")
 
 if __name__ == "__main__":
     main()
